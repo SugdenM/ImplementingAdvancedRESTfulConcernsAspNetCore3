0ea1ae60a4ab37e21e1737824e202760a733e4a4
Mark Sugden <mark.andrew.sugden@outlook.com>
2020-07-13 14:47:27 +0100

    Cleaned upd PageList

--- Starter files/CourseLibrary/CourseLibrary.API/Helpers/PagedList.cs
+++ Starter files/CourseLibrary/CourseLibrary.API/Helpers/PagedList.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Helpers
 {
-    public class PagedList<T>: List<T>
+    public class PagedList<T> : List<T>
     {
-        public int CurrentPage { get; private set; }
-        public int TotalPages { get; private set; }
-        public int PageSize { get; private set; }
-        public int TotalCount { get; private set; }
-
-        public bool HasPrevious => (CurrentPage > 1);
-        public bool HasNext => (CurrentPage < TotalPages);
-
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
             TotalCount = count;
             PageSize = pageSize;
             CurrentPage = pageNumber;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize);
             AddRange(items);
         }
+
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public int TotalCount { get; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
 
         public static PagedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
         {
