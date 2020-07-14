2557aba9f9d8bbc5e156bae9d05c0cda18e725b4
Mark Sugden <mark.andrew.sugden@outlook.com>
2020-07-14 11:00:19 +0100

    Add Http Cache Support

--- Starter files/CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
+++ Starter files/CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using CourseLibrary.API.Entities;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -15,7 +16,9 @@ namespace CourseLibrary.API.Controllers
 {
     [ApiController]
     [Route("api/authors/{authorId}/courses")]
-    [ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
+    [HttpCacheExpiration(CacheLocation = CacheLocation.Public)]
+    [HttpCacheValidation(MustRevalidate = true)]
+    //[ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
     public class CoursesController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -40,7 +43,9 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
-        [ResponseCache(Duration = 120)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Public, MaxAge = 1000)]
+        [HttpCacheValidation(MustRevalidate = false)]
+        //[ResponseCache(Duration = 120)]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId)) return NotFound();
--- Starter files/CourseLibrary/CourseLibrary.API/Startup.cs
+++ Starter files/CourseLibrary/CourseLibrary.API/Startup.cs
@@ -1,19 +1,21 @@
+using System;
+using System.Linq;
 using AutoMapper;
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CourseLibrary.API
 {
@@ -29,83 +31,86 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHttpCacheHeaders(expirationModelOptions =>
+                {
+                    expirationModelOptions.MaxAge = 60;
+                    expirationModelOptions.CacheLocation = CacheLocation.Private;
+                },
+                validationModelOptions => { validationModelOptions.MustRevalidate = true; });
             services.AddResponseCaching();
 
             services.AddControllers(setupAction =>
-            {
-                setupAction.ReturnHttpNotAcceptable = true;
-                setupAction.CacheProfiles.Add("240SecondsCacheProfile",
-                    new CacheProfile{ Duration = 240});
-
-            }).AddNewtonsoftJson(setupAction =>
-             {
-                 setupAction.SerializerSettings.ContractResolver =
-                    new CamelCasePropertyNamesContractResolver();
-             })
-            .AddXmlDataContractSerializerFormatters()
-            .ConfigureApiBehaviorOptions(setupAction =>
-            {
-                setupAction.InvalidModelStateResponseFactory = context =>
                 {
-                    // create a problem details object
-                    var problemDetailsFactory = context.HttpContext.RequestServices
-                        .GetRequiredService<ProblemDetailsFactory>();
-                    var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                    setupAction.ReturnHttpNotAcceptable = true;
+                    setupAction.CacheProfiles.Add("240SecondsCacheProfile",
+                        new CacheProfile {Duration = 240});
+                }).AddNewtonsoftJson(setupAction =>
+                {
+                    setupAction.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                })
+                .AddXmlDataContractSerializerFormatters()
+                .ConfigureApiBehaviorOptions(setupAction =>
+                {
+                    setupAction.InvalidModelStateResponseFactory = context =>
+                    {
+                        // create a problem details object
+                        var problemDetailsFactory = context.HttpContext.RequestServices
+                            .GetRequiredService<ProblemDetailsFactory>();
+                        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
                             context.HttpContext,
                             context.ModelState);
 
-                    // add additional info not added by default
-                    problemDetails.Detail = "See the errors field for details.";
-                    problemDetails.Instance = context.HttpContext.Request.Path;
+                        // add additional info not added by default
+                        problemDetails.Detail = "See the errors field for details.";
+                        problemDetails.Instance = context.HttpContext.Request.Path;
 
-                    // find out which status code to use
-                    var actionExecutingContext =
-                          context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+                        // find out which status code to use
+                        var actionExecutingContext =
+                            context as ActionExecutingContext;
 
-                    // if there are modelstate errors & all keys were correctly
-                    // found/parsed we're dealing with validation errors
-                    //
-                    // if the context couldn't be cast to an ActionExecutingContext
-                    // because it's a ControllerContext, we're dealing with an issue 
-                    // that happened after the initial input was correctly parsed.  
-                    // This happens, for example, when manually validating an object inside
-                    // of a controller action.  That means that by then all keys
-                    // WERE correctly found and parsed.  In that case, we're
-                    // thus also dealing with a validation error.
-                    if (context.ModelState.ErrorCount > 0 &&
-                        (context is ControllerContext ||
-                         actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
-                    {
-                        problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
-                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
-                        problemDetails.Title = "One or more validation errors occurred.";
-
-                        return new UnprocessableEntityObjectResult(problemDetails)
+                        // if there are modelstate errors & all keys were correctly
+                        // found/parsed we're dealing with validation errors
+                        //
+                        // if the context couldn't be cast to an ActionExecutingContext
+                        // because it's a ControllerContext, we're dealing with an issue 
+                        // that happened after the initial input was correctly parsed.  
+                        // This happens, for example, when manually validating an object inside
+                        // of a controller action.  That means that by then all keys
+                        // WERE correctly found and parsed.  In that case, we're
+                        // thus also dealing with a validation error.
+                        if (context.ModelState.ErrorCount > 0 &&
+                            (context is ControllerContext ||
+                             actionExecutingContext?.ActionArguments.Count ==
+                             context.ActionDescriptor.Parameters.Count))
                         {
-                            ContentTypes = { "application/problem+json" }
-                        };
-                    }
+                            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "One or more validation errors occurred.";
 
-                    // if one of the keys wasn't correctly found / couldn't be parsed
-                    // we're dealing with null/unparsable input
-                    problemDetails.Status = StatusCodes.Status400BadRequest;
-                    problemDetails.Title = "One or more errors on input occurred.";
-                    return new BadRequestObjectResult(problemDetails)
-                    {
-                        ContentTypes = { "application/problem+json" }
+                            return new UnprocessableEntityObjectResult(problemDetails)
+                            {
+                                ContentTypes = {"application/problem+json"}
+                            };
+                        }
+
+                        // if one of the keys wasn't correctly found / couldn't be parsed
+                        // we're dealing with null/unparsable input
+                        problemDetails.Status = StatusCodes.Status400BadRequest;
+                        problemDetails.Title = "One or more errors on input occurred.";
+                        return new BadRequestObjectResult(problemDetails)
+                        {
+                            ContentTypes = {"application/problem+json"}
+                        };
                     };
-                };
-            });
+                });
 
             services.Configure<MvcOptions>(config =>
             {
                 var newtonSofJsonOutputFormatter = config.OutputFormatters
                     .OfType<NewtonsoftJsonOutputFormatter>()?.FirstOrDefault();
                 if (newtonSofJsonOutputFormatter != null)
-                {
                     newtonSofJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.marvin.hateoas+json");
-                }
             });
 
             // Register PropertyMappingsService
@@ -128,11 +133,8 @@ namespace CourseLibrary.API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
                 app.UseExceptionHandler(appBuilder =>
                 {
                     appBuilder.Run(async context =>
@@ -142,18 +144,15 @@ namespace CourseLibrary.API
                     });
                 });
 
-            }
+            //app.UseResponseCaching();
 
-            app.UseResponseCaching();
+            app.UseHttpCacheHeaders();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
